a76a5de72f86872ec2340ace85205117a0287a3c
SungYeolha <tjddufgk@gmail.com>
2021-10-15 02:50:24 +0900

    Add global-styles, (Home, Detail) route

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import { GlobalStyles } from './global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
