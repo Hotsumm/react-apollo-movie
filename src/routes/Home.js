30887e4ce97add5ca3c3406dfcd683bbc39d3bf3
SungYeolha <tjddufgk@gmail.com>
2021-10-15 03:44:45 +0900

    Add GET MOVIES query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,29 @@
-const Home = () => 'Home';
+import React from 'react';
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        data &&
+        data.movies &&
+        data.movies.map((movie, index) => <h1 key={index}>{movie.id}</h1>)
+      )}
+    </div>
+  );
+};
 export default Home;
--- src/routes/apollo.js
+++ src/routes/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  uri: 'https://graphql-movie-api-2021.herokuapp.com',
   cache: new InMemoryCache(),
 });
 
