e762e0b1274c8ce2f752e04776ca8b1f7043e60d
SungYeolha <tjddufgk@gmail.com>
2021-10-15 03:17:54 +0900

    Apollo Client

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { GlobalStyles } from './global-styles';
+import { ApolloProvider } from '@apollo/client';
+import client from './routes/apollo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <App />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
